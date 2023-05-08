6fcfe6418177b39500a3ce79b724c452528cef02
José Ricardo Ferreira de Souza <josericardoferreiradesouza13@gmail.com>
2023-05-08 11:52:00 -0300

    Apaguei carai

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-    int contador = 0;
-    while (contador<10){
-        System.out.println("contador = " + contador);
 
-        contador =  contador +1;
-    }
-    int cont= 1;
-    do {
-        System.out.println(cont);
-        cont++;
-    }while(cont <= 10);
-
-
-    for(int X = 0; X <= 10000000; X++);{
-            System.out.println(cont);
-        }
-
-    }
+}
 }
